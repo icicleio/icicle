a28e23c5ad747657f579a9dba4041a8a656e0c86
Aaron Piotrowski <aaron@trowski.com>
2014-08-12 18:51:53 -0500

    Update.

---
+++ examples/echo.php
@@ -0,0 +1,27 @@
+<?php
+
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+use Icicle\Coroutine\Coroutine;
+use Icicle\Loop\Loop;
+use Icicle\Socket\Server;
+use Icicle\Socket\Client;
+use Icicle\Socket\Exception\TimeoutException;
+
+$coroutine = Coroutine::call(function () {
+    $server = Server::create('localhost', 8080);
+    
+    $handler = Coroutine::async(function (Client $client) {
+        try {
+            yield $client->pipe($client);
+        } finally {
+            $client->close();
+        }
+    });
+    
+    while ($server->isOpen()) {
+        $handler(yield $server->accept());
+    }
+});
+
+Loop::run();
