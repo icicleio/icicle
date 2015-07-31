c20cd6cb7da7c02335e4e095e926f2e8ac71d4c7
Aaron Piotrowski <aaron@trowski.com>
2015-07-31 17:08:26 -0500

    Add file header

--- src/Promise/Structures/FulfilledPromise.php
+++ src/Promise/Structures/FulfilledPromise.php
@@ -1,4 +1,12 @@
 <?php
+
+/*
+ * This file is part of Icicle, a library for writing asynchronous code in PHP using promises and coroutines.
+ *
+ * @copyright 2014-2015 Aaron Piotrowski. All rights reserved.
+ * @license Apache-2.0 See the LICENSE file that was distributed with this source code for more information.
+ */
+
 namespace Icicle\Promise\Structures;
 
 use Icicle\Loop;
