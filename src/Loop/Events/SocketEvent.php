8fcdc44b7dd36117ed7283e61b63823f023d6751
Aaron Piotrowski <aaron@trowski.com>
2015-09-14 18:49:22 -0500

    Update file headers

--- src/Loop/Events/SocketEvent.php
+++ src/Loop/Events/SocketEvent.php
@@ -4,7 +4,7 @@
  * This file is part of Icicle, a library for writing asynchronous code in PHP using promises and coroutines.
  *
  * @copyright 2014-2015 Aaron Piotrowski. All rights reserved.
- * @license Apache-2.0 See the LICENSE file that was distributed with this source code for more information.
+ * @license MIT See the LICENSE file that was distributed with this source code for more information.
  */
 
 namespace Icicle\Loop\Events;
