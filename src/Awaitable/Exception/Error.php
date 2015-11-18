74bc17cbf7993f361c241f9a34d8680922c7b837
Aaron Piotrowski <aaron@trowski.com>
2015-11-18 10:15:26 -0600

    Update file headers

--- src/Awaitable/Exception/Error.php
+++ src/Awaitable/Exception/Error.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * This file is part of Icicle, a library for writing asynchronous code in PHP using promises and coroutines.
+ * This file is part of Icicle, a library for writing asynchronous code in PHP using coroutines built with awaitables.
  *
  * @copyright 2014-2015 Aaron Piotrowski. All rights reserved.
  * @license MIT See the LICENSE file that was distributed with this source code for more information.
