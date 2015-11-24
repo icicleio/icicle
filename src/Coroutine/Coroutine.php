ffdb1c9a9609b1710d96012be3d1c7cbcd871066
Aaron Piotrowski <aaron@trowski.com>
2015-11-23 20:43:32 -0600

    Check for synchronous continuation; remove ability to pause a coroutine

--- src/Coroutine/Coroutine.php
+++ src/Coroutine/Coroutine.php
@@ -11,36 +11,31 @@ namespace Icicle\Coroutine;
 
 use Exception;
 use Generator;
-use Icicle\Awaitable\Future;
 use Icicle\Awaitable\Awaitable;
+use Icicle\Awaitable\Future;
 use Icicle\Loop;
 
 /**
- * This class implements cooperative coroutines using Generators. Coroutines should yield promises to pause execution
- * of the coroutine until the promise has resolved. If the promise is fulfilled, the fulfillment value is sent to the
- * generator. If the promise is rejected, the rejection exception is thrown into the generator.
+ * This class implements cooperative coroutines using Generators. Coroutines should yield awaitables to pause execution
+ * of the coroutine until the awaitable has resolved. If the awaitable is fulfilled, the fulfillment value is sent to
+ * the generator. If the awaitable is rejected, the rejection exception is thrown into the generator.
  */
 final class Coroutine extends Future
 {
     /**
-     * @var \Generator|null
+     * @var \Generator
      */
     private $generator;
 
     /**
-     * @var \Closure|null
+     * @var \Closure
      */
     private $send;
     
     /**
-     * @var \Closure|null
+     * @var \Closure
      */
     private $capture;
-    
-    /**
-     * @var mixed[]|null
-     */
-    private $next;
 
     /**
      * @var bool
@@ -60,8 +55,8 @@ final class Coroutine extends Future
          * @param mixed $value The value to send to the generator.
          */
         $this->send = function ($value = null) {
-            if ($this->paused) { // If paused, save callable and value for resuming.
-                $this->next = [$this->send, $value];
+            if ($this->paused) { // Avoid blowing up the call stack by queuing the continuation.
+                Loop\queue($this->send, $value);
                 return;
             }
 
@@ -70,7 +65,6 @@ final class Coroutine extends Future
                 $this->next($this->generator->send($value), $value);
             } catch (Exception $exception) {
                 $this->reject($exception);
-                $this->close();
             }
         };
 
@@ -78,8 +72,8 @@ final class Coroutine extends Future
          * @param \Exception $exception Exception to be thrown into the generator.
          */
         $this->capture = function (Exception $exception) {
-            if ($this->paused) { // If paused, save callable and exception for resuming.
-                $this->next = [$this->capture, $exception];
+            if ($this->paused) { // Avoid blowing up the call stack by queuing the continuation.
+                Loop\queue($this->capture, $exception);
                 return;
             }
 
@@ -88,7 +82,6 @@ final class Coroutine extends Future
                 $this->next($this->generator->throw($exception));
             } catch (Exception $exception) {
                 $this->reject($exception);
-                $this->close();
             }
         };
 
@@ -96,7 +89,6 @@ final class Coroutine extends Future
             $this->next($this->generator->current());
         } catch (Exception $exception) {
             $this->reject($exception);
-            $this->close();
         }
     }
 
@@ -110,9 +102,10 @@ final class Coroutine extends Future
     {
         if (!$this->generator->valid()) {
             $this->resolve($last);
-            $this->close();
             return;
         }
+
+        $this->paused = true;
 
         if ($yielded instanceof Generator) {
             $yielded = new self($yielded);
@@ -123,53 +116,8 @@ final class Coroutine extends Future
         } else {
             Loop\queue($this->send, $yielded);
         }
-    }
 
-    /**
-     * The garbage collector does not automatically detect (at least not quickly) the circular references that can be
-     * created, so explicitly setting these parameters to null is necessary for proper freeing of memory.
-     */
-    private function close()
-    {
-        $this->next = null;
-        $this->send = null;
-        $this->capture = null;
-
-        $this->paused = true;
-
-        $this->generator = null;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function pause()
-    {
-        $this->paused = true;
-    }
-    
-    /**
-     * {@inheritdoc}
-     */
-    public function resume()
-    {
-        if ($this->isPending() && $this->paused) {
-            $this->paused = false;
-            
-            if (null !== $this->next) {
-                list($callable, $value) = $this->next;
-                Loop\queue($callable, $value);
-                $this->next = null;
-            }
-        }
-    }
-    
-    /**
-     * {@inheritdoc}
-     */
-    public function isPaused()
-    {
-        return $this->paused;
+        $this->paused = false;
     }
 
     /**
@@ -177,16 +125,10 @@ final class Coroutine extends Future
      */
     public function cancel(Exception $reason = null)
     {
-        if (null !== $this->generator) {
+        if ($this->isPending()) {
             $current = $this->generator->current(); // Get last yielded value.
             if ($current instanceof Awaitable) {
                 $current->cancel($reason); // Cancel last yielded awaitable.
-            }
-
-            try {
-                $this->close(); // Throwing finally blocks in the Generator may cause close() to throw.
-            } catch (Exception $exception) {
-                $reason = $exception;
             }
         }
 
