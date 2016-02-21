0cc59c0bc07c6171b259fbfe4df80168a78f9769
Aaron Piotrowski <aaron@trowski.com>
2016-02-21 13:31:47 -0600

    Revert fix only meant for PHP 5.5

--- src/Observable/functions.php
+++ src/Observable/functions.php
@@ -196,15 +196,10 @@ if (!function_exists(__NAMESPACE__ . '\from')) {
             try {
                 yield Awaitable\choose($coroutines);
                 yield $delayed;
-
+            } finally {
                 foreach ($coroutines as $coroutine) {
                     $coroutine->cancel();
                 }
-            } catch (\Exception $exception) {
-                foreach ($coroutines as $coroutine) {
-                    $coroutine->cancel($exception);
-                }
-                throw $exception;
             }
 
             return $i; // Return the number of times a set was emitted.
